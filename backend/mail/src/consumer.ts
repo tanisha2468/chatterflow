9f4216cedc904d77b2ecd83bd8751e8de8d70bdf
tanisha2468 <tanisha2468@users.noreply.github.com>
2026-02-14 20:40:00 +0000

    Add mail service: RabbitMQ consumer for OTP emails

---
+++ backend/mail/src/consumer.ts
@@ -0,0 +1,56 @@
+import amqp from "amqplib";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const startSendOtpConsumer = async () => {
+  try {
+    const connection = await amqp.connect({
+      protocol: "amqp",
+      hostname: process.env.RABBITMQ_HOST,
+      port: 5672,
+      username: process.env.RABBITMQ_USERNAME,
+      password: process.env.RABBITMQ_PASSWORD,
+    });
+
+    const channel = await connection.createChannel();
+
+    const queueName = "send-otp";
+
+    await channel.assertQueue(queueName, { durable: true });
+
+    console.log("✅ Mail Service consumer started, listening for otp emails");
+
+    channel.consume(queueName, async (msg) => {
+      if (msg) {
+        try {
+          const { to, subject, body } = JSON.parse(msg.content.toString());
+
+          const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, //for aws
+            auth: {
+              user: process.env.EMAIL_USER,
+              pass: process.env.EMAIL_PASS,
+            },
+          });
+
+          await transporter.sendMail({
+            from: "Chat app",
+            to,
+            subject,
+            text: body,
+          });
+
+          console.log(`OTP mail sent to ${to}`);
+          channel.ack(msg);
+        } catch (error) {
+          console.log("Failed to send otp", error);
+        }
+      }
+    });
+  } catch (error) {
+    console.log("Failed to start rabbitmq consumer", error);
+  }
+};
